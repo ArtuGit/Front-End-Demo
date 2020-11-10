45ee0f9fb825d6b40980405fb457aab24e79b702
Artem Laptiev <artuon@gmail.com>
2020-11-10 23:29:46 +0200

    EasyBank Landing Page: Adopt the updated tailwind boilerplate

--- easybank-landing-page-master/tailwind.config.js
+++ easybank-landing-page-master/tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    mode: 'all',
+    content: [
+      // Paths to your templates here...
+    ],
+  },
   theme: {
-    extend: {},
+    screens: {
+      'sm': {'min': '1px', 'max': '767px'},
+      'md': {'min': '768px'},
+      'lg': {'min': '1024px'},
+      'xl': {'min': '1280px'},
+    },
   },
   variants: {},
   plugins: [],
