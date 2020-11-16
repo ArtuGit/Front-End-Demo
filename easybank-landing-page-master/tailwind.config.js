fc63d559934d051bec8b359156e955617de79fa8
Artem L <artuon@gmail.com>
2020-11-16 22:47:53 +0200

    Easybank landing-page: Developing "Articles" section

--- easybank-landing-page-master/tailwind.config.js
+++ easybank-landing-page-master/tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
     screens: {
       'sm': {'min': '1px', 'max': '767px'},
       'md': {'min': '768px'},
-      'lg': {'min': '1024px'},
-      'xl': {'min': '1280px'},
+      'lg': {'min': '1200px'},
+      'xl': {'min': '1500px'},
     },
     colors: {
       'cst-primary': {
