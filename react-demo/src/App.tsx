12005cc24825ff9ff2728832a6533ec558a16edd
Artu <artuon@gmail.com>
2024-09-03 00:20:00 +0200

    Box

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  return (
-      <>
-          <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end h-screen">
-              <h1 className="text-white text-3xl font-bold">Gradient Background</h1>
-          </div>
-      </>
-  )
+    return (
+        <>
+            <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end h-screen">
+                <div className="flex justify-center items-center h-full">
+                    <div className="w-1/6 h-1/6 border-4 border-dashed border-gray-200 rounded-lg"></div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
