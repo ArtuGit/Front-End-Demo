847bc0b40af84274dd7d0c67cf57218ee6c4a620
Artu <artuon@gmail.com>
2024-09-02 23:45:48 +0200

    Colors

--- react-demo/tailwind.config.js
+++ react-demo/tailwind.config.js
@@ -10,6 +10,11 @@ export default {
         'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
       },
       colors: {
+        'peach': '#ffeaae',
+        'amber': '#ffc100',
+        'flush-orange': '#ff8200',
+        'red': '#ff0000',
+        'karaka': '#0a0903',
         'bg-gradient-start': '#a9d6e5',
         'bg-gradient-end': '#89c2d9',
       },
