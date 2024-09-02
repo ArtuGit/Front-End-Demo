f3a70cf69548823dcc2c8a3e7fc12c5b68212b93
Artu <artuon@gmail.com>
2024-09-02 23:35:09 +0200

    Bg gradient

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end h-screen">
+              <h1 className="text-white text-3xl font-bold">Gradient Background</h1>
+          </div>
       </>
   )
 }
--- react-demo/tailwind.config.js
+++ react-demo/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'bg-gradient-start': '#a9d6e5',
+        'bg-gradient-end': '#89c2d9',
+      },
+    },
   },
   plugins: [],
 }
