58a2ca0aab0139eb49db2bcf53bfd399db2d1515
Artu <artuon@gmail.com>
2024-09-03 22:36:37 +0200

    Components

--- react-demo/src/components/BaseBox/Box.tsx
+++ react-demo/src/components/BaseBox/Box.tsx
@@ -1,6 +1,6 @@
 import '../../App.css'
 
-export const BaseBox = () => {
+export const Box = () => {
     return (
         <div className="w-1/6 h-1/6 border-4 border-dashed border-gray-200 rounded-lg"></div>
     );
--- react-demo/src/components/Boxes/Boxes.tsx
+++ react-demo/src/components/Boxes/Boxes.tsx
@@ -1,10 +1,10 @@
 import '../../App.css'
-import { BaseBox } from "../BaseBox/BaseBox.tsx";
+import { Box } from "../BaseBox/Box.tsx";
 
 export const Boxes = () => {
     return (
         <div className="flex justify-center items-center h-full">
-            <BaseBox/>
+            <Box/>
         </div>
     );
 };
