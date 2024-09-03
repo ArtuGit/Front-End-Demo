b9730ad55e9592faf8e0fa7be1d315abf7684c27
Artu <artuon@gmail.com>
2024-09-03 22:59:07 +0200

    Box

--- react-demo/src/components/BaseBox/Box.tsx
+++ react-demo/src/components/BaseBox/Box.tsx
@@ -1,7 +1,14 @@
 import '../../App.css'
+import {useState} from "react";
 
 export const Box = () => {
+    const [isSelected, setIsSelected] = useState<boolean>(false);
+
     return (
-        <div className="w-1/6 h-1/6 border-4 border-dashed border-gray-200 rounded-lg"></div>
+        <div
+          className={`w-52 h-52 border-4 border-dashed border-gray-200 rounded-lg ${isSelected ? 'bg-blue-400' : ''}`}
+          onClick={() => setIsSelected(!isSelected)}
+        >
+        </div>
     );
 };
