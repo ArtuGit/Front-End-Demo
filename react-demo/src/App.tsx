b11ffd93fd37be199916df7ad3e333fc0c1cc597
Artu <artuon@gmail.com>
2024-09-06 19:27:07 +0200

    Semantic

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -4,9 +4,9 @@ import  { Boxes } from './components/Boxes/Boxes'
 function App() {
     return (
         <>
-            <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end min-h-screen h-max">
+            <section className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end min-h-screen h-max">
                 <Boxes amount={15}/>
-            </div>
+            </section>
         </>
     )
 }
