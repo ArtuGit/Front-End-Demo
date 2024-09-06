5d11ab7d008d562aa055e5f5609d0ccfd6c013f2
Artu <artuon@gmail.com>
2024-09-06 11:56:32 +0200

    Style

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -4,7 +4,7 @@ import  { Boxes } from './components/Boxes/Boxes'
 function App() {
     return (
         <>
-            <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end h-screen">
+            <div className="bg-gradient-to-b from-bg-gradient-start to-bg-gradient-end min-h-screen h-max">
                 <Boxes amount={15}/>
             </div>
         </>
--- react-demo/src/components/Boxes/Boxes.tsx
+++ react-demo/src/components/Boxes/Boxes.tsx
@@ -12,7 +12,7 @@ export const Boxes = ({ amount }: BoxesProps) => {
     }
 
     return (
-        <div className="flex flex-wrap justify-center items-center h-full">
+        <div className="flex flex-wrap justify-center items-center min-h-screen">
             {boxes}
         </div>
     );
