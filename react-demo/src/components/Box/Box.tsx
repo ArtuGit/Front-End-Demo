8254f6a6ad3aa6a675f65246403ec2d3f3556e4a
Artu <artuon@gmail.com>
2024-09-07 13:36:35 +0200

    Stat

--- react-demo/src/components/Box/Box.tsx
+++ react-demo/src/components/Box/Box.tsx
@@ -1,17 +1,16 @@
 import '../../App.css'
-import { useState } from "react";
 
 interface BoxProps {
     index: number;
+    isSelected: boolean;
+    setBoxesItem: (index: number) => void;
 }
 
-export const Box = ({ index }: BoxProps) => {
-    const [isSelected, setIsSelected] = useState<boolean>(false);
-
+export const Box = ({ index, isSelected, setBoxesItem }: BoxProps) => {
     return (
         <div
             className={`w-52 h-52 mx-4 my-4 border-4 border-dashed border-gray-200 rounded-lg ${isSelected ? 'bg-blue-400' : ''}`}
-            onClick={() => setIsSelected((selected) => !selected)}
+            onClick={() => setBoxesItem(index)}
         >
         </div>
     );
--- react-demo/src/components/Boxes/Boxes.tsx
+++ react-demo/src/components/Boxes/Boxes.tsx
@@ -1,24 +1,36 @@
 import '../../App.css'
 import { Box } from "../Box/Box.tsx";
+import {useState} from "react";
 
 interface BoxesProps {
     amount: number;
 }
 
 export const Boxes = ({ amount }: BoxesProps) => {
-    const boxes = [];
+    const [boxes, setBoxes] = useState<boolean[]>(new Array(amount).fill(false));
+
+    const  setBoxesItem = (index: number) => {
+        setBoxes((prevBoxes) => {
+            const newBoxes = [...prevBoxes];
+            newBoxes[index] = !newBoxes[index];
+            return newBoxes;
+        });
+    }
+
+    const boxesComponents = [];
+
     for (let i = 0; i < amount; i++) {
-        boxes.push(<Box key={i} index={i}/>);
+        boxesComponents.push(<Box key={i} index={i} isSelected={boxes[i]} setBoxesItem={setBoxesItem}/>);
     }
 
     return (
         <div className="flex-col flex-nowrap">
             <div className="flex flex-wrap justify-center items-center">
-                {boxes}
+                {boxesComponents}
             </div>
             <div className="my-4 flex flex-wrap justify-center">
                 <div className="font-robomo mx-8">Total: <span className="font-bold">{amount}</span></div>
-                <div className="font-robomo mx-8">Selected: <span className="font-bold">0</span></div>
+                <div className="font-robomo mx-8">Selected: <span className="font-bold">{boxes.filter(isSelected => isSelected).length}</span></div>
             </div>
         </div>
     );
